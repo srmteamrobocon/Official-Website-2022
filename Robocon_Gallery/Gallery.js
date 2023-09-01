a4f8bd7f66f18d908a5d95203d2f560795e0a4bc
Pranav Malakar <pranav.malakar@gmail.com>
2023-09-01 23:32:53 +0530

    Update

--- Robocon_Gallery/Gallery.js
+++ Robocon_Gallery/Gallery.js
@@ -5,7 +5,7 @@ var numImages = 52;
 var galleryContainer = document.getElementById('Robocon_Gallery');
 var ulElement = document.createElement('ul');
 ulElement.className = 'gallery';
-for (var i = 1; i <= numImages; i++) {
+for (var i = numImages; i > 0; i++) {
     var imagePath = 'Robocon_Gallery/Robocon_Images_'+i+'.jpg';
     var altText = 'Image ' + i;
     var liElement = document.createElement('li');
